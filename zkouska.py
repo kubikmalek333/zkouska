72947c9d6584194756f830ab1010ebfd8a0405c7
kubikmalek333 <jakubmalek84@gmail.com>
2026-09-17 12:16:00 +0200

    Initial commit

--- zkouska.py
+++ zkouska.py
@@ -1,5 +1,5 @@
 import os
  
-with open(os.sep.join(["P:/2.B-Pro/soubor", "Text.txt"]), encoding="utf-8") as soubor:
+with open(os.sep.join(["Text.txt"]), encoding="utf-8") as soubor:
     obsah = soubor.read()
     print(obsah)
